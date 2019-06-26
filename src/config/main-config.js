c0fe4213f7e3eb15441f177f8187ad057e87de80
JackWSargent <jackwsargent@gmail.com>
2019-06-26 11:30:25 -0400

    took out sessions

--- src/config/main-config.js
+++ src/config/main-config.js
@@ -12,12 +12,12 @@ module.exports = {
         app.set("views", viewsFolder);
         app.set("view engine", "ejs");
         app.use(bodyParser.urlencoded({ extended: true }));
-        app.use(session({
-            secret: process.env.cookieSecret,
-            resave: false,
-            saveUninitialized: false,
-            cookie: { maxAge: 1.21e+9 } //set cookie to expire in 14 days
-        }));
+        // app.use(session({
+        //     secret: process.env.cookieSecret,
+        //     resave: false,
+        //     saveUninitialized: false,
+        //     cookie: { maxAge: 1.21e+9 } //set cookie to expire in 14 days
+        // }));
         app.use(flash());
         passportConfig.init(app);
         // app.use((req,res,next) => {
