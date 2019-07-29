8c6414e45a99214b292cea4703efcecae891b5f1
JackWSargent <jackwsargent@gmail.com>
2019-07-29 01:49:08 -0400

    fixed downgrades

--- spec/integration/wiki_spec.js
+++ spec/integration/wiki_spec.js
@@ -102,24 +102,24 @@ describe("routes : wikis", () => {
                 });
             });
         });
-        // describe("POST /wikis/:id/destroy", () => {
-        //     it("should not delete the wiki with the associated ID", (done) => {
-        //         Wiki.findAll()
-        //         .then((wikis) => {
-        //             const wikiCountBeforeDelete = wikis.length;
-        //             expect(wikiCountBeforeDelete).toBe(1);
-        //             request.post(`${base}${this.wiki.id}/destroy`, (err, res, body) => {
-        //                 Wiki.findAll()
-        //                 .then((wikis) => {
-        //                     //console.log(wikis);
-        //                     expect(err).toBeNull();
-        //                     expect(wikis.length).toBe(wikiCountBeforeDelete);
-        //                     done();
-        //                 })
-        //             });
-        //         });
-        //     });
-        // });
+        describe("POST /wikis/:id/destroy", () => {
+            it("should not delete the wiki with the associated ID", (done) => {
+                Wiki.findAll()
+                .then((wikis) => {
+                    const wikiCountBeforeDelete = wikis.length;
+                    expect(wikiCountBeforeDelete).toBe(1);
+                    request.post(`${base}${this.wiki.id}/destroy`, (err, res, body) => {
+                        Wiki.findAll()
+                        .then((wikis) => {
+                            //console.log(wikis);
+                            expect(err).toBeNull();
+                            expect(wikis.length).toBe(wikiCountBeforeDelete - 1);
+                            done();
+                        })
+                    });
+                });
+            });
+        });
         describe("GET /wikis/:id/edit", () => {
             it("should not render a view with an edit wiki form", (done) => {
                 request.get(`${base}${this.wiki.id}/edit`, (err, res, body) => {
@@ -153,13 +153,13 @@ describe("routes : wikis", () => {
             });
         });
     });
-    describe("admin tests for wiki CRUD", () => {
+    describe("premium tests for wiki CRUD", () => {
         beforeEach((done) => {
             User.create({
                 username: "userhneil",
                 email: "person211@example.com",
                 password: "123456",
-                role: 0
+                role: 1
             })
             .then((user) => {
                 request.get({         
